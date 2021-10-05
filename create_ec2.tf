c968878b972f8f3361455b8a8ca59004ce8e154f
luanbt <luanbt@gmail.com>
2021-10-05 15:36:25 +0700

    Update create_ec2.tf

--- create_ec2.tf
+++ create_ec2.tf
@@ -1,18 +1,10 @@
-module "ec2_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 3.0"
-
-  name = "aws_ec2_test"
-
-  ami                    = "ami-082105f875acab993"
-  instance_type          = "t2.micro"
-  key_name               = "user1"
-  monitoring             = true
-  vpc_security_group_ids = ["sg-01e83ab0e67250ded"]
-  subnet_id              = "subnet-cc9713aa"
-
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
+resource "aws_instance" "testinstance" {
+	ami = "ami-082105f875acab994"
+	instance_type = "t2.micro"
+	subnet_id = "subnet-cc9713aa"
+	vpc_security_group_ids = [ "sg-01e83ab0e67250ded" ]
+	key_name "testinstance"
+tags {
+	"Name" = "testinstance"
+     }
 }
