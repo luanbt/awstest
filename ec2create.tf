7ffb095cdc98d6f0ed992aef0ce1b54cb20f8030
luanbt <luanbt@gmail.com>
2021-10-05 16:59:18 +0700

    Update ec2create.tf

--- ec2create.tf
+++ ec2create.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "testinstance" {
-	ami = "ami-082105f875acab994"
+	ami = "ami-082105f875acab993"
 	instance_type = "t2.micro"
 	subnet_id = "subnet-cc9713aa"
 	vpc_security_group_ids = [ "sg-01e83ab0e67250ded" ]	
